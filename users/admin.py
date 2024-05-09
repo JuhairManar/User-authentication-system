f1c4d23e0d9e2f1985064dc7c0b3ce827545bc74
MD JUHAIAR MANAR CHOWDHURY <89413439+JuhairManar@users.noreply.github.com>
2024-05-09 06:43:56 +0600

    Update admin.py

--- users/admin.py
+++ users/admin.py
@@ -10,11 +10,11 @@ class Blogadmin(admin.ModelAdmin):
     list_display=['title','category','truncated_summary', 'truncated_content','save_as_draft']
     
     def truncated_summary(self,obj):
-        words = obj.summary.split()
+        words=obj.summary.split()
         if len(words) > 5: 
-            truncated_summary = ' '.join(words[:5]) + '...'  
+            truncated_summary=' '.join(words[:5]) + '...'  
         else:
-            truncated_summary = ' '.join(words) 
+            truncated_summary=' '.join(words) 
             
         return truncated_summary
     
